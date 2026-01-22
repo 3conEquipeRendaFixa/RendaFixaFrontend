e1479cff8d38e86a2e67decf00fdeb11c39f420b
Enzo <enzo.victor@trescon.com.br>
2026-01-22 14:33:37 -0300

    feat(calendar-grid): adding calendar grid component

---
+++ src/app/domain/calendar/components/calendar-grid/calendar-grid.ts
@@ -0,0 +1,24 @@
+import { Component, input, output } from '@angular/core';
+import { CalendarItem } from '@shared/interfaces/calendar.interface';
+import { ButtonIcon } from 'widget/components/buttons/button-icon/button-icon';
+
+@Component({
+  selector: 'srf-b3-calendar-grid',
+  standalone: true,
+  imports: [ButtonIcon],
+  templateUrl: './calendar-grid.html',
+  styleUrl: './calendar-grid.scss',
+})
+export class CalendarGrid {
+  readonly items = input<CalendarItem[]>([]);
+
+  readonly rowClick = output<CalendarItem>();
+
+  onRowClick(item: CalendarItem): void {
+    this.rowClick.emit(item);
+  }
+
+  formatBoolean(value: boolean): string {
+    return value ? 'Sim' : 'Não';
+  }
+}
