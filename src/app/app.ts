6ef0cf8aa8b2081e333bfce493619b40e36cab82
Enzo <enzo.victor@trescon.com.br>
2026-01-22 14:36:57 -0300

    feat: adding eslint to project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,8 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'srf-b3-root',
   imports: [RouterOutlet],
   styleUrl: './app.scss',
   template: '<router-outlet></router-outlet>',
