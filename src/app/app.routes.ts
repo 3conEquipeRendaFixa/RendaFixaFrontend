8bf2542e73ae55bcc5b4d2ab9cf77f5b11cb042e
Enzo <enzo.victor@trescon.com.br>
2026-01-22 13:38:41 -0300

    feat(calendar.routes, app.routes): adding calendar routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'calendar',
+        loadChildren: () => import('@domain/calendar/calendar.routes').then(m => m.CALENDAR_ROUTES)
+    }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -7,6 +7,4 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss',
   template: '<router-outlet></router-outlet>',
 })
-export class App {
-  
-}
+export class App {}
